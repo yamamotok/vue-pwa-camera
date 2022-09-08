792f7c7b8ceb34e30ee10ab52ef18a6e1994df2b
Keisuke Yamamoto <2731953+yamamotok@users.noreply.github.com>
2022-09-08 12:57:52 +0900

    Create "capture" route

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,14 @@
 
 <template>
   <section>
-    <h1>Camera / PWA / Vue</h1>
-    <div>
+    <h1 class="mb-4">Camera / PWA / Vue</h1>
+    <div class="m-4 text-center">
+      <h2>video approach</h2>
       <router-link to="/camera" type="button" class="btn btn-primary">Start Camera</router-link>
+    </div>
+    <div class="m-4 text-center">
+      <h2>input/capture approach</h2>
+      <router-link to="/capture" type="button" class="btn btn-primary">Start Camera</router-link>
     </div>
   </section>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import './style.css';
 import App from './App.vue';
 import Home from './components/Home.vue';
 import Camera from './components/Camera.vue';
+import Capture from './components/Capture.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/camera', name: 'Camera', component: Camera },
+    { path: '/capture', name: 'Capture', component: Capture },
   ],
 });
 
